e7c2694e9b164e78fe993452ab69fc5dfbaa5a68
acroisie <acroisie@student.42lyon.fr>
2024-12-11 18:10:27 +0100

    Improve scatter_plot visualisation

--- scatter_plot.py
+++ scatter_plot.py
@@ -10,6 +10,14 @@ def on_close(event):
 if __name__ == "__main__":
     dataset = Dataset("./data/dataset_train.csv")
     features = dataset.get_features()
+    houses = dataset.get_houses()
+
+    house_colors = {
+        "Gryffindor": "red",
+        "Ravenclaw": "blue",
+        "Slytherin": "green",
+        "Hufflepuff": "orange",
+    }
 
     plt.ion()
     fig, ax = plt.subplots()
@@ -21,22 +29,31 @@ if __name__ == "__main__":
                 feature_x = features[i]
                 feature_y = features[j]
 
-                x_values = []
-                y_values = []
-                for row in dataset.get_data():
-                    try:
-                        x_val = float(row[feature_x])
-                        y_val = float(row[feature_y])
-                        x_values.append(x_val)
-                        y_values.append(y_val)
-                    except ValueError:
-                        continue
-
                 ax.clear()
-                ax.scatter(x_values, y_values, alpha=0.5)
+                for house in houses:
+                    x_values = []
+                    y_values = []
+                    for row in dataset.get_data():
+                        if row["Hogwarts House"] == house:
+                            try:
+                                x_val = float(row[feature_x])
+                                y_val = float(row[feature_y])
+                                x_values.append(x_val)
+                                y_values.append(y_val)
+                            except ValueError:
+                                continue
+
+                    ax.scatter(
+                        x_values,
+                        y_values,
+                        alpha=0.5,
+                        label=house,
+                    )
+
                 ax.set_title(f"{feature_x} vs {feature_y}")
                 ax.set_xlabel(feature_x)
                 ax.set_ylabel(feature_y)
+                ax.legend()
                 plt.draw()
 
                 input("Press [enter] to show the next pair of features...")
