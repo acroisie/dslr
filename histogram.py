72f4a776203533614198776153956eedba563b16
acroisie <acroisie@student.42lyon.fr>
2024-12-11 15:54:12 +0100

    Working histogram.py

--- histogram.py
+++ histogram.py
@@ -6,16 +6,28 @@ if __name__ == "__main__":
     features = dataset.get_features()
     houses = dataset.get_houses()
 
+    plt.ion()
+    _, ax = plt.subplots()
+
+try:
     for feature in features:
         house_values = dataset.values_by_house(feature)
+        ax.clear()
 
-    plt.figure(figsize=(8, 6))
-    for house in houses:
-        plt.hist(house_values[house], bins=30, alpha=0.5, label=house)
+        for house in houses:
+            ax.hist(house_values[house], bins=30, alpha=0.5, label=house)
 
-    plt.title(f"Distribution of {feature} by Hogwarts House")
-    plt.xlabel(feature)
-    plt.ylabel("Number of Students")
-    plt.legend()
-    plt.tight_layout()
-    plt.show()
+        ax.set_title(f"Distribution of {feature} by Hogwarts House")
+        ax.set_xlabel(feature)
+        ax.set_ylabel("Number of Students")
+        ax.legend()
+
+        plt.draw()
+        input("Press [enter] to show next feature...")
+
+except KeyboardInterrupt:
+    print("\nExiting...")
+
+finally:
+    plt.ioff()
+    plt.close("all")
