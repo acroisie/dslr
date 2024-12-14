1db068a54a089bce61c15bcf6c7b1b197ebbabc7
Arnaud <acroisie@student.42lyon.fr>
2024-12-14 18:36:05 +0100

    Add MathUtils

--- math_utils.py
+++ math_utils.py
@@ -1,11 +1,33 @@
 import math
 
+class MathUtils:
+    @staticmethod
+    def sigmoid(z):
+        return 1 / (1 + math.exp(-z))
 
-def sigmoid(z):
-    return 1 / (1 + math.exp(-z))
+    @staticmethod
+    def compute_tendency(X, theta):
+        z = sum(X[i] * theta[i] for i in range(len(theta)))
+        return MathUtils.sigmoid(z)
 
-def compute_tendency(X, theta):
-    z = sum(X[i] * theta[i] for i in range(len(theta)))
-    return sigmoid(z)
+    @staticmethod
+    def compute_cost(X, y, theta):
+        m = len(X)
+        total = 0.0
+        for i in range(m):
+            h = MathUtils.compute_tendency(X[i], theta)
+            total += -y[i] * math.log(h) - (1 - y[i]) * math.log(1 - h)
+        return total / m
 
-def compute_cost(X, y, theta)
+    @staticmethod
+    def compute_gradient(X, y, theta):
+        m = len(X)
+        grad = [0.0] * len(theta)
+        for i in range(m):
+            h = MathUtils.compute_tendency(X[i], theta)
+            error = h - y[i]
+            for j in range(len(theta)):
+                grad[j] += error * X[i][j]
+        for j in range(len(theta)):
+            grad[j] /= m
+        return grad
